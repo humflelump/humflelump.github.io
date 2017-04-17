e71aa69eab82ef7d93278303a0c3d70cc195987b
Ian Philips <iansp@cs.utexas.edu>
2017-04-17 14:34:27 -0500

    added new file structure to comply with flask python commands/functions

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask # Import Flask package
+from flask import render_template # Import render_template function
+app = Flask(__name__) # Construct Flask object named 'app'
+
+'''
+@app.route() defines the URLs that route to the index() function.
+The URLs will be appended to the end of the base URL.
+Links within HTML files should use the defined routes (for example '/index') as
+the values for href attributes.
+
+URLs that will call the index() function if running app.py on localhost:
+	http://localhost:5000/
+	http://localhost:5000/index
+'''
+@app.route('/') # URL for function (default for home page)
+@app.route('/index') # Secondary URL for function
+def index():
+	return render_template('index.html') # located in templates/
+
+if __name__ == "__main__":
+    app.run()
+
+
